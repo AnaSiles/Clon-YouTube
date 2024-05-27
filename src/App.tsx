fd6adc5b7cc12700ad5053590939c39126594695
AnaSiles <anasiles_232@yahoo.com>
2024-05-27 17:22:07 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { PageHeader } from "./layouts/PageHeader"
+
+function App() {
+
+
+  return (
+    <div className="max-h-screen flex flex-col">
+      <PageHeader />
+      <div></div>
+    </div>
+  )
+}
+
+export default App
