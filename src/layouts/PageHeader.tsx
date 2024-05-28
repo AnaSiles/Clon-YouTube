17c0a03c02c76bf556fcd08a7b88b540be6e86a3
AnaSiles <anasiles_232@yahoo.com>
2024-05-28 09:29:00 +0200

    Añadir responsive del search y añadir logo  la perstaña de navegación

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,10 +1,13 @@
-import { Bell, Menu, UploadIcon, User } from "lucide-react"
+import { ArrowLeft, Bell, Menu, Mic, Search, UploadIcon, User } from "lucide-react"
 import logo from "../assets/MyTubeLogo.png" 
 import { Button } from "../components/Button"
+import { useState } from "react"
 
 export function PageHeader() {
+    const [showFullWithSearch, setShowFullWidthSearch]= useState(false)
+
     return (<div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
-        <div className="flex gap-4 items-center flex-shrink-0">
+        <div className={`gap-4 items-center flex-shrink-0 ${showFullWithSearch? "hidden" : "flex"}`}>
             <Button variant="ghost" size="icon" >
                 <Menu />
             </Button>
@@ -12,8 +15,30 @@ export function PageHeader() {
                 <img src={logo} alt="Logo" className="h-12" />
             </a>
         </div>
-        <div></div>
-        <div className="flex flex-shrink-0 md:gap-2">
+        <form 
+        className={`gap-4 flex-grow justify-center ${showFullWithSearch ? "flex" : "hidden md:flex"}`}>
+            {showFullWithSearch && (<Button onClick={()=> setShowFullWidthSearch(false)} type="button" size="icon" variant="ghost" className="flex-shrink-0">
+                <ArrowLeft/>
+            </Button>
+            )}
+            <div className="flex flex-grow max-w-[600px]">
+                <input type="search" placeholder = "Search" className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary
+                py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"/>
+                <Button className="py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0">
+                    <Search/>
+                </Button>
+            </div>
+            <Button type="button" size="icon" className="flex-shrink-0">
+                <Mic/>
+            </Button>
+        </form>
+        <div className={`flex-shrink-0 md:gap-2 ${showFullWithSearch ? "hidden" : "flex"}`	}>
+            <Button onClick={() => setShowFullWidthSearch(true)} size= "icon" variant="ghost" className="md:hidden">
+                <Search/>
+            </Button>
+            <Button size= "icon" variant="ghost" className="md:hidden">
+                <Mic/>
+            </Button>
             <Button size= "icon" variant="ghost">
                 <UploadIcon/>
             </Button>
